fa6196bc97c9594391b0a17c044254e270b9d5ca
cesar <cesar@cesar-Inspiron-3520>
2018-03-20 00:27:23 -0500

    Sobreescribe el Controller para Usuario Usa un form diferente entre crear y modificar

---
+++ src/AppBundle/Form/UsuarioModificarType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class UsuarioModificarType extends AbstractType {
+	/**
+	 *
+	 * {@inheritdoc}
+	 *
+	 */
+	public function buildForm(FormBuilderInterface $builder, array $options) {
+	    $builder->get('plainPassword')->setRequired(false);
+	}
+
+	public function getExistingRoles() {
+		return array (
+				'Secretario' => 'ROLE_SECRETARIO',
+				'Miembro de junta' => 'ROLE_MIEMBRO_DE_JUNTA',
+				'Psicólogo' => 'ROLE_PSICOLOGO',
+				'Administrador de Junta' => 'ROLE_ADMIN_JUNTA',
+				'Super Admin' => 'ROLE_SUPER_ADMIN' 
+		);
+	}
+	public function getParent() {
+	    return UsuarioType::class;
+	}
+}
